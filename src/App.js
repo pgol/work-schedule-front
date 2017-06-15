be20c419e1c3171dcd0654dc73806feec1683988
pgol <pgol200014@gmail.com>
2017-06-15 14:24:47 +0200

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, {Component} from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 import './App.css';
 import UsersView from  './components/users-view';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <UsersView />
-      </div>
+        <Router>
+          <div className="App">
+            <ul>
+              <li>
+                <Link to="/">Users</Link>
+              </li>
+            </ul>
+            <Route path="/" component={UsersView} />
+          </div>
+        </Router>
     );
   }
 }
