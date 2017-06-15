97632f817f9944b1dac5be070b5a72f278dca5d8
Dizlu <golmarcin@gmail.com>
2017-06-15 14:49:27 +0200

    Adding code without redux, saga etc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
 import UsersView from  './components/users-view';
+import Event from './components/Event/Event';
+import { events } from './tools/tools';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <UsersView />
+        { events.map((event, index) => (
+          <Event event={event}/>
+        ))}
       </div>
     );
   }
