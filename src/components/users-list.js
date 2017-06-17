e85b155c350b1f8b874f970bc3d94937242fde55
pgol <pgol200014@gmail.com>
2017-06-17 16:08:48 +0200

    Add prop types

--- src/components/users-list.js
+++ src/components/users-list.js
@@ -1,23 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function usersList({users}) {
-  let usersItems;
-
-  if (users.length > 0) {
-    usersItems = users.map(item => (
-      <li key={item.id}>{item.username} @{item.email}</li>
-    ))
-  } else {
-    usersItems = [];
+  function getUsers(users) {
+    if (users.length > 0) {
+      return (
+        <ul>
+          {users.map(item => (
+            <li key={item.id}>{item.username} @{item.email}</li>
+          ))}
+        </ul>
+      );
+    } else {
+      return (
+        <div>No users loaded</div>
+      )
+    }
   }
 
   return (
     <div>
       <ul>
-        {usersItems}
+        {getUsers(users)}
       </ul>
     </div>
   )
 }
+
+usersList.propTypes = {
+  users: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    username: PropTypes.string,
+    email: PropTypes.string
+  }))
+};
 
 export default usersList;
